cb5917ac91664b38a4edc6a72f06f0609acfc50f
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-11-12 11:31:07 +0200

    login-token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,5 +11,5 @@ const { validateBody } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 router.post("/register", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register));
-
+router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login));
 module.exports = router;
