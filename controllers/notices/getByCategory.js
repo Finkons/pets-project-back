c64608f4dd911c63ffda1196db9f2349e8d0d864
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-11-12 16:36:38 +0200

    paginat

--- controllers/notices/getByCategory.js
+++ controllers/notices/getByCategory.js
@@ -4,8 +4,9 @@ const { RequestError } = require("../../helpers");
 
 const getByCategory = async (req, res) => {
   const { category } = req.params;
-
-  const result = await Notice.find({ category }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Notice.find({ category }, "-createdAt -updatedAt", { skip, limit });
   if (!result) {
     throw RequestError(404, "Not found");
   }
