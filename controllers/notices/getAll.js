d9af1b1925d6fae88628654ad7923aecd3f32b7f
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-11-14 22:15:20 +0200

    query/notices

--- controllers/notices/getAll.js
+++ controllers/notices/getAll.js
@@ -1,9 +1,10 @@
 const { Notice } = require("../../models/notice");
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, ...query } = req.query;
+  console.log(query);
   const skip = (page - 1) * limit;
-  const result = await Notice.find({}, "-createdAt -updatedAt", { skip, limit });
+  const result = await Notice.find({ ...query }, "-createdAt -updatedAt", { skip, limit });
   res.status(200).json(result);
 };
 
