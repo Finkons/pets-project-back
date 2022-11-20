f974a4d71bf343b5b892f81d2523aed5e46edec5
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-11-20 18:27:55 +0200

    fixed query

--- controllers/notices/getByCategory.js
+++ controllers/notices/getByCategory.js
@@ -4,9 +4,17 @@ const { RequestError } = require("../../helpers");
 
 const getByCategory = async (req, res) => {
   const { category } = req.params;
-  const { page = 1, limit = 10, ...query } = req.query;
+  const { query } = req.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Notice.find({ category, ...query }, "-createdAt -updatedAt", { skip, limit });
+  if (!query) {
+    const result = await Notice.find({ category }, "-createdAt -updatedAt", { skip, limit });
+    if (result.length !== 0) {
+      return res.status(200).json(result);
+    }
+    throw RequestError(404, "Not found");
+  }
+  const result = await Notice.find({ category, title: { $regex: `${query}` } }, "-createdAt -updatedAt", { skip, limit });
   if (result.length !== 0) {
     return res.status(200).json(result);
   }
