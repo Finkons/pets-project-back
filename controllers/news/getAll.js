4094b525a9fc1b8d3da503e35a5d36176108b03c
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-11-27 21:59:38 +0200

    sort added

--- controllers/news/getAll.js
+++ controllers/news/getAll.js
@@ -4,13 +4,13 @@ const { RequestError } = require("../../helpers");
 const getAll = async (req, res) => {
   const { query } = req.query;
   if (!query) {
-    const result = await News.find();
+    const result = await News.find().sort("-date");
     if (result.length !== 0) {
       return res.status(200).json(result);
     }
     throw RequestError(404, "Not found");
   }
-  const result = await News.find({ title: { $regex: `${query}` } });
+  const result = await News.find({ title: { $regex: `${query}` } }).sort("-date");
   if (result.length !== 0) {
     return res.status(200).json(result);
   }
