99783a59be0433eeb2990582e4d33effc151b697
Andrey <andrey@MacBook-Air-Andrey.local>
2022-11-29 10:15:13 +0000

    commented getOwn and getFevorite

--- controllers/notices/getFavorite.js
+++ controllers/notices/getFavorite.js
@@ -1,13 +1,13 @@
 const { Notice } = require("../../models/notice");
-const { RequestError } = require("../../helpers");
+// const { RequestError } = require("../../helpers");
 
 const getFavorite = async (req, res) => {
   const { _id: userId } = req.user;
   const result = await Notice.find({ fans: userId._id });
-  if (result.length !== 0) {
+  // if (result.length !== 0) {
     return res.status(200).json(result);
-  }
-  throw RequestError(404, "Not found");
+  // }
+  // throw RequestError(404, "Not found");
 };
 
 module.exports = getFavorite;
--- controllers/notices/getOwn.js
+++ controllers/notices/getOwn.js
@@ -1,13 +1,13 @@
 const { Notice } = require("../../models/notice");
-const { RequestError } = require("../../helpers");
+// const { RequestError } = require("../../helpers");
 
 const getOwn = async (req, res) => {
   const { _id: owner } = req.user;
   const result = await Notice.find({ owner });
-  if (result.length !== 0) {
-    return res.status(200).json(result);
-  }
-  throw RequestError(404, "Not found");
+  // if (result.length !== 0) {
+  return res.status(200).json(result);
+  // }
+  // throw RequestError(404, "Not found");
 };
 
 module.exports = getOwn;
