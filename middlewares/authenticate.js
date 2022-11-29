63841bca74164eea54b3c009f9c40c068b076df4
Andrey <andrey@MacBook-Air-Andrey.local>
2022-11-29 12:38:20 +0000

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,21 +8,18 @@ const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer = "", token = ""] = authorization.split(" ");
     if (bearer !== "Bearer") {
-      // throw RequestError(401, "Not authorized");
-      throw console.log("Not authorized")
+      throw RequestError(401, "Not authorized");
     }
     try {
       const { id } = jwt.verify(token, SECRET_KEY);
       const user = await User.findById(id);
       if (!user || !user.token) {
-        // throw Error("Not authorized");
-        throw console.log("Not authorized")
+        throw Error("Not authorized");
       }
       req.user = user;
       next();
     } catch (error) {
-      // throw RequestError(401, "Not authorized");
-      throw console.log("Not authorized")
+      throw RequestError(401, "Not authorized");
     }
   } catch (error) {
     next(error);
